31ec99d54ec481007db8ebd4da2e01235aac2904
Manoj Kumar Reddy Pollagari <thejonamreddy@gmail.com>
2024-09-29 16:22:23 +0530

    Added course add api

---
+++ src/app/api/course/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server"
+import { createClient } from "@supabase/supabase-js"
+import { Course } from "@/app/interfaces/course"
+
+const supabaseUrl = process.env.SUPABASE_URL || ""
+const supabaseKey = process.env.SUPABASE_KEY || ""
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams
+  const id = searchParams.get('id')
+  const { data, error } = await supabase
+    .from('courses')
+    .select()
+    .eq('id', id)
+
+  return NextResponse.json(data![0], { status: 200 });
+}
+
+export async function POST(req: NextRequest) {
+  const { title, description, targetAudience, learningObjectives, level, duration } = await req.json() as Course
+
+  const { data, error } = await supabase
+    .from('courses')
+    .insert({
+      title,
+      description,
+      targetAudience,
+      learningObjectives,
+      level, 
+      duration
+    })
+    .select()
+
+  return NextResponse.json(data![0], { status: 200 });
+}
