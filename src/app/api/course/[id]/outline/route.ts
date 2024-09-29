d3e8eb87b2cac6045de1cd74d30c77aab45ae90c
Manoj Kumar Reddy Pollagari <thejonamreddy@gmail.com>
2024-09-30 01:28:45 +0530

    Outline get and set api

--- src/app/api/course/[id]/outline/route.ts
+++ src/app/api/course/[id]/outline/route.ts
@@ -1,71 +1,6 @@
 import { NextRequest, NextResponse } from "next/server"
-import { GoogleGenerativeAI, SchemaType } from "@google/generative-ai";
 import { createClient } from "@supabase/supabase-js";
 import { Topic } from "@/app/interfaces/topic";
-
-const schema = {
-  type: SchemaType.ARRAY,
-  description: "List of topics",
-  items: {
-    type: SchemaType.OBJECT,
-    description: "Details of the topic",
-    properties: {
-      key: {
-        type: SchemaType.STRING,
-        description: "Key of the topic"
-      },
-      topic: {
-        type: SchemaType.STRING,
-        description: "Name of the topic"
-      },
-      description: {
-        type: SchemaType.STRING,
-        description: "Description of what this topic covers"
-      },
-      selected: {
-        type: SchemaType.BOOLEAN,
-        description: "Is always true"
-      },
-      subTopics: {
-        type: SchemaType.ARRAY,
-        description: "List of sub topics",
-        items: {
-          type: SchemaType.OBJECT,
-          description: "Details of the sub topic",
-          properties: {
-            key: {
-              type: SchemaType.STRING,
-              description: "Key of the sub topic"
-            },
-            subTopic: {
-              type: SchemaType.STRING,
-              description: "Name of the sub topic"
-            },
-            description: {
-              type: SchemaType.STRING,
-              description: "Description of what this sub topic covers"
-            },
-            selected: {
-              type: SchemaType.BOOLEAN,
-              description: "Is always true"
-            },
-          },
-          required: ["key", "subTopic", "description", "selected"]
-        }
-      }
-    },
-    required: ["key", "topic", "subTopics", "description", "selected"]
-  }
-};
-
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_CLOUD_API_KEY || "");
-const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
-  generationConfig: {
-    responseMimeType: "application/json",
-    responseSchema: schema,
-  },
-});
 
 const supabaseUrl = process.env.SUPABASE_URL || ""
 const supabaseKey = process.env.SUPABASE_KEY || ""
@@ -74,44 +9,19 @@ const supabase = createClient(supabaseUrl, supabaseKey)
 export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
   const { data: topics } = await supabase
     .from('topics')
+    .select('*, audios(id)')
+    .eq('courseId', params.id)
+
+  const { data: subTopics } = await supabase
+    .from('subtopics')
     .select()
     .eq('courseId', params.id)
 
-  /* Check for Saved Data */
-  if (topics?.length) {
-    const { data: subTopics } = await supabase
-      .from('subtopics')
-      .select()
-      .eq('courseId', params.id)
+  topics!.forEach((t) => {
+    t.subTopics = (subTopics || []).filter((st) => st.topicId === t.id)
+  })
 
-    topics.forEach((t) => {
-      t.subTopics = (subTopics || []).filter((st) => st.topicId === t.id)
-    })
-
-    return NextResponse.json(topics, { status: 200 });
-  }
-
-  const { data, error } = await supabase
-    .from('courses')
-    .select()
-    .eq('id', params.id)
-  const { title, description, targetAudience, learningObjectives, level, duration } = data![0]
-
-  const prompt = `
-  Forget everything we've talked about.
-
-  Suggest a course outline for a course with 
-  Title: ${title}
-  Description: ${description}
-  Target Audience: ${targetAudience}
-  Learning Objectives: ${learningObjectives}
-  Level: ${level}
-  Duration: ${duration}
-  `;
-
-  const result = await model.generateContent(prompt);
-  const jsonResult = JSON.parse(result.response.text())
-  return NextResponse.json(jsonResult, { status: 200 });
+  return NextResponse.json(topics, { status: 200 });
 }
 
 export async function POST(req: NextRequest, { params }: { params: { id: string } }) {
@@ -149,8 +59,12 @@ export async function POST(req: NextRequest, { params }: { params: { id: string 
         courseId: params.id,
         topicId: data![0].id
       })))
+    /* Update Course Status */
+    const { } = await supabase
+      .from('courses')
+      .update({ status: 'outlined' })
+      .eq('id', params.id)
   }
 
   return NextResponse.json(null, { status: 200 })
-
 }
