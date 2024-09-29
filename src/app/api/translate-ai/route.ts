0f81d65d9c3f49799980118f2b3faea75190e92a
Manoj Kumar Reddy Pollagari <thejonamreddy@gmail.com>
2024-09-30 01:16:12 +0530

    Added translate api

---
+++ src/app/api/translate-ai/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server"
+
+export async function POST(req: NextRequest) {
+  const { text, sourceLanguage, targetLanguage } = await req.json()
+  
+  const response = await fetch('https://api.sarvam.ai/translate', {
+    method: 'POST',
+    headers: {
+      'API-Subscription-Key': process.env.SARVAM_API_KEY || "",
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      input: text,
+      source_language_code: sourceLanguage,
+      target_language_code: targetLanguage,
+      enable_preprocessing: true
+    })
+  })
+
+  const result = await response.json();
+  return NextResponse.json(result.translated_text, { status: 200 });
+}
