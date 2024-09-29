a6c7ba293bdd15754731b81cebfda8d199e0151d
Manoj Kumar Reddy Pollagari <thejonamreddy@gmail.com>
2024-09-30 01:29:35 +0530

    Api to get topic by id

---
+++ src/app/api/topic/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server"
+import { createClient } from "@supabase/supabase-js"
+
+const supabaseUrl = process.env.SUPABASE_URL || ""
+const supabaseKey = process.env.SUPABASE_KEY || ""
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams
+  const id = searchParams.get('id')
+  const { data, error } = await supabase
+    .from('topics')
+    .select()
+    .eq('id', id)
+
+  return NextResponse.json(data![0], { status: 200 });
+}
