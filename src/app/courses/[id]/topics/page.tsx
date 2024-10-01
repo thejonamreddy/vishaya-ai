4b0f86ba34af33cc97157256e41b50f78ded8443
Manoj Kumar Reddy Pollagari <thejonamreddy@gmail.com>
2024-10-01 19:42:06 +0530

    Topics new UI (updated)

---
+++ src/app/courses/[id]/topics/page.tsx
@@ -0,0 +1,155 @@
+'use client'
+
+import { Topic } from "@/app/interfaces/topic"
+import { LoaderCircle, Save, WandSparkles } from "lucide-react"
+import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
+import { Course } from "@/app/interfaces/course"
+import { TopicModel } from "@/app/models/topic"
+import TopicSelection from "./topic-selection"
+import { Button } from "@/components/ui/button"
+
+export default function Topics({ params }: { params: { id: string } }) {
+  const router = useRouter()
+
+  const [loading, setLoading] = useState(true)
+  const [course, setCourse] = useState<Course>()
+  const [aiGenerated, setAIGenerated] = useState(false)
+  const [topics, setTopics] = useState<TopicModel[]>([])
+
+  function listToTree(data: Topic[]): TopicModel[] {
+    const map: { [id: string]: TopicModel } = {}
+    const tree = [] as TopicModel[]
+
+    data.forEach(({ id, title, description, selected }) => {
+      map[id] = { title, description, selected, children: [] }
+    })
+
+    data.forEach(item => {
+      if (item.parentId) {
+        map[item.parentId].children.push(map[item.id])
+      } else {
+        tree.push(map[item.id])
+      }
+    })
+
+    return tree
+  }
+
+  async function loadData() {
+    try {
+      setLoading(true)
+
+      const courseResponse = await fetch(`/api/course?id=${params.id}`)
+      const courseData = await courseResponse.json()
+
+      const topicsResponse = await fetch(`/api/course/${params.id}/topic`)
+      const topicsData = await topicsResponse.json() as Topic[]
+
+      if (topicsData.length) {
+        setAIGenerated(false)
+        const topicModels = listToTree(topicsData)
+        setTopics(topicModels)
+      } else {
+        setAIGenerated(true)
+        const aiTopicsResponse = await fetch(`/api/topic-ai`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(courseData)
+        })
+        const aiTopics = await aiTopicsResponse.json() as TopicModel[]
+        setTopics(aiTopics)
+      }
+
+      setCourse(courseData)
+    } catch {
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    if (params.id) {
+      loadData()
+    }
+  }, [])
+
+  function checkForSelected(nodes: TopicModel[]) {
+    nodes.forEach((topic) => {
+      if (topic.children) {
+        topic.selected = topic.children?.some((t) => t.selected)
+        checkForSelected(topic.children)
+      }
+    })
+  }
+
+  function onTopicToggle(topic: TopicModel, checked: boolean) {
+    topic.selected = checked
+    topic.children?.forEach((t) => {
+      t.selected = checked
+    })
+    checkForSelected(topics)
+    setTopics([...topics])
+  }
+
+  async function generateTopics() {
+    try {
+      setLoading(true)
+      const response = await fetch(`/api/topic-ai`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(course)
+      })
+      const data = await response.json() as TopicModel[]
+      setTopics(data)
+    } catch {
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  async function save() {
+    try {
+      setLoading(true)
+      const { } = await fetch(`/api/course/${params.id}/topic`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(topics)
+      })
+      router.push(`/courses/${params.id}/contents`)
+    } catch {
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      {loading && !course && !topics.length ? (
+        <LoaderCircle className="h-6 w-6 animate-spin" />
+      ) : (
+        <div className="flex flex-col gap-4">
+          <TopicSelection loading={loading} topics={topics} topicToggle={onTopicToggle} />
+          <div className="flex gap-4">
+            <Button variant="outline" className="flex items-center gap-4" disabled={loading} onClick={generateTopics}>
+              {!loading && <WandSparkles className="h-4 w-4" />}
+              {loading && <LoaderCircle className="h-4 w-4 animate-spin" />}
+              Generate Topics
+            </Button>
+            <Button className="flex items-center gap-4" disabled={loading} onClick={save}>
+              {!loading && <Save className="h-4 w-4" />}
+              {loading && <LoaderCircle className="h-4 w-4 animate-spin" />}
+              Save
+            </Button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
---
+++ src/app/courses/[id]/topics/topic-selection.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { TopicModel } from "@/app/models/topic";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import { useState } from "react";
+
+interface Props {
+  loading: boolean,
+  topics: TopicModel[],
+  topicToggle(topic: TopicModel, checked: boolean): void
+}
+
+export default function TopicSelection({ loading, topics, topicToggle }: Props) {
+  const [toggleDesc, setToggleDesc] = useState(true)
+
+  function Topic(topic: TopicModel) {
+    return (
+      <div className="flex items-start gap-4 bg-white p-4 border rounded-md">
+        <Checkbox checked={topic.selected} disabled={loading} onCheckedChange={(e) => topicToggle(topic, e)} />
+        <div className="flex flex-col gap-2 w-full">
+          <Label>{topic.title}</Label>
+          {toggleDesc && <p className="text-sm text-muted-foreground">{topic.description}</p>}
+          {topic.children?.map((child, i) => (
+            <div key={i}>{Topic(child)}</div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      {/* Toggle Description */}
+      <div className="flex items-center gap-4">
+        <Switch id="toggle-desc" checked={toggleDesc} onCheckedChange={setToggleDesc} disabled={loading} />
+        <Label htmlFor="toggle-desc">Toggle Description</Label>
+      </div>
+      {topics.map((topic, i) => (
+        <div key={i}>{Topic(topic)}</div>
+      ))}
+    </div>
+  )
+}
