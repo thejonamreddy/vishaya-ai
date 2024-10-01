a5ca93f4a8a978439fe9ab2e51aa43f34980941c
Manoj Kumar Reddy Pollagari <thejonamreddy@gmail.com>
2024-10-01 16:05:33 +0530

    Changes to course new pagw

--- src/app/courses/new/form.tsx
+++ src/app/courses/new/form.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
@@ -19,6 +18,7 @@ import { useForm } from "react-hook-form";
 import { Course } from "@/app/interfaces/course";
 import { LoaderCircle } from "lucide-react";
 import { Checkbox } from "@/components/ui/checkbox";
+import { Language } from "@/app/interfaces/language";
 
 export const CourseFormSchema = z.object({
   title: z.string({ required_error: "Title is required" }),
@@ -34,11 +34,12 @@ export const CourseFormSchema = z.object({
 
 interface Props {
   loading: boolean,
+  languages: Language[],
   course?: Course,
   submit(formData: z.infer<typeof CourseFormSchema>): void
 }
 
-export default function CourseForm({ loading, course, submit }: Props) {
+export default function CourseForm({ loading, languages, course, submit }: Props) {
   const targetAudiences = [
     'Business Professionals',
     'IT Professionals',
@@ -65,24 +66,10 @@ export default function CourseForm({ loading, course, submit }: Props) {
     '7 - 16 Hours',
     '17+ Hours']
 
-    const languages = [
-      { id: "en-IN", label: "English (India)", disabled: true }, 
-      { id: "hi-IN", label: "Hindi (India)" }, 
-      { id: "bn-IN", label: "Bangla (India)" }, 
-      { id: "kn-IN", label: "Kannada (India)" }, 
-      { id: "ml-IN", label: "Malayalam (India)" }, 
-      { id: "mr-IN", label: "Marathi (India)" }, 
-      { id: "od-IN", label: "Odia (India)" }, 
-      { id: "pa-IN", label: "Punjabi (India)" }, 
-      { id: "ta-IN", label: "Tamil (India)" }, 
-      { id: "te-IN", label: "Telugu (India)" }, 
-      { id: "gu-IN", label: "Gujarati (India)" }, 
-    ]
-
   const form = useForm<z.infer<typeof CourseFormSchema>>({
     resolver: zodResolver(CourseFormSchema),
     defaultValues: course || {
-      languages: ["en-IN"],
+      languages: [languages.find((l) => l.code === 'en-IN')?.id],
     },
     disabled: loading
   })
@@ -94,172 +81,166 @@ export default function CourseForm({ loading, course, submit }: Props) {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)}>
-        <div className="grid gap-4">
-          <Card>
-            <CardContent className="pt-6">
-              <div className="grid gap-6">
-                {/* Title */}
-                <FormField
-                  control={form.control}
-                  name="title"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel className="text-inherit">Title</FormLabel>
-                      <FormControl>
-                        <Input type="text" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                {/* Description */}
-                <FormField
-                  control={form.control}
-                  name="description"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel className="text-inherit">Description</FormLabel>
-                      <FormControl>
-                        <Textarea {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                {/* Target Audience */}
-                <FormField
-                  control={form.control}
-                  name="targetAudience"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel className="text-inherit">Target Audience</FormLabel>
-                      <Select onValueChange={field.onChange} defaultValue={field.value} disabled={loading}>
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          {targetAudiences.map((x, i) => (
-                            <SelectItem key={i} value={x}>{x}</SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                {/* Learning Objectives */}
-                <FormField
-                  control={form.control}
-                  name="learningObjectives"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel className="text-inherit">Learning Objectives</FormLabel>
-                      <FormControl>
-                        <Textarea {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                {/* Level */}
-                <FormField
-                  control={form.control}
-                  name="level"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel className="text-inherit">Level</FormLabel>
-                      <Select onValueChange={field.onChange} defaultValue={field.value} disabled={loading}>
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          {levels.map((x, i) => (
-                            <SelectItem key={i} value={x}>{x}</SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                {/* Duration */}
-                <FormField
-                  control={form.control}
-                  name="duration"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel className="text-inherit">Duration</FormLabel>
-                      <Select onValueChange={field.onChange} defaultValue={field.value} disabled={loading}>
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          {durations.map((x, i) => (
-                            <SelectItem key={i} value={x}>{x}</SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                {/* Languages */}
-                <FormField
-                  control={form.control}
-                  name="languages"
-                  render={() => (
-                    <FormItem>
-                      <div className="mb-4">
-                        <FormLabel className="text-base">Languages</FormLabel>
-                      </div>
-                      {languages.map((lang) => (
-                        <FormField
-                          key={lang.id}
-                          control={form.control}
-                          name="languages"
-                          render={({ field }) => {
-                            return (
-                              <FormItem key={lang.id} className="flex flex-row items-start space-x-3 space-y-0">
-                                <FormControl>
-                                  <Checkbox
-                                    disabled={lang.disabled || loading}
-                                    checked={field.value?.includes(lang.id)}
-                                    onCheckedChange={(checked) => {
-                                      return checked
-                                        ? field.onChange([...field.value, lang.id])
-                                        : field.onChange(
-                                          field.value?.filter(
-                                            (value) => value !== lang.id
-                                          )
-                                        )
-                                    }}
-                                  />
-                                </FormControl>
-                                <FormLabel className="font-normal">
-                                  {lang.label}
-                                </FormLabel>
-                              </FormItem>
-                            )
-                          }}
-                        />
-                      ))}
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button className="w-full flex items-center gap-4" type="submit" disabled={loading}>
-                  Generate
-                  {loading && <LoaderCircle className="h-4 w-4 animate-spin" />}
-                </Button>
-              </div>
-            </CardContent>
-          </Card>
+        <div className="bg-white p-4 border rounded-md flex flex-col gap-4">
+          {/* Title */}
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-inherit">Title</FormLabel>
+                <FormControl>
+                  <Input type="text" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Description */}
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-inherit">Description</FormLabel>
+                <FormControl>
+                  <Textarea {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Target Audience */}
+          <FormField
+            control={form.control}
+            name="targetAudience"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-inherit">Target Audience</FormLabel>
+                <Select onValueChange={field.onChange} defaultValue={field.value} disabled={loading}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {targetAudiences.map((x, i) => (
+                      <SelectItem key={i} value={x}>{x}</SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Learning Objectives */}
+          <FormField
+            control={form.control}
+            name="learningObjectives"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-inherit">Learning Objectives</FormLabel>
+                <FormControl>
+                  <Textarea {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Level */}
+          <FormField
+            control={form.control}
+            name="level"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-inherit">Level</FormLabel>
+                <Select onValueChange={field.onChange} defaultValue={field.value} disabled={loading}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {levels.map((x, i) => (
+                      <SelectItem key={i} value={x}>{x}</SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Duration */}
+          <FormField
+            control={form.control}
+            name="duration"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-inherit">Duration</FormLabel>
+                <Select onValueChange={field.onChange} defaultValue={field.value} disabled={loading}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {durations.map((x, i) => (
+                      <SelectItem key={i} value={x}>{x}</SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Languages */}
+          <FormField
+            control={form.control}
+            name="languages"
+            render={() => (
+              <FormItem>
+                <div className="mb-4">
+                  <FormLabel className="text-base">Languages</FormLabel>
+                </div>
+                {languages.map(({ id, code, name }) => (
+                  <FormField
+                    key={id}
+                    control={form.control}
+                    name="languages"
+                    render={({ field }) => {
+                      return (
+                        <FormItem key={id} className="flex flex-row items-start space-x-3 space-y-0">
+                          <FormControl>
+                            <Checkbox
+                              disabled={loading || code === 'en-IN'}
+                              checked={field.value?.includes(id)}
+                              onCheckedChange={(checked) => {
+                                return checked
+                                  ? field.onChange([...field.value, id])
+                                  : field.onChange(
+                                    field.value?.filter(
+                                      (value) => value !== id
+                                    )
+                                  )
+                              }}
+                            />
+                          </FormControl>
+                          <FormLabel className="font-normal">
+                            {name}
+                          </FormLabel>
+                        </FormItem>
+                      )
+                    }}
+                  />
+                ))}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className="w-full flex items-center gap-4" type="submit" disabled={loading}>
+            {loading && <LoaderCircle className="h-4 w-4 animate-spin" />}
+            Save
+          </Button>
         </div>
       </form>
     </Form>
--- src/app/courses/new/page.tsx
+++ src/app/courses/new/page.tsx
@@ -3,25 +3,53 @@
 import { z } from "zod";
 import CourseForm, { CourseFormSchema } from "./form";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Language } from "@/app/interfaces/language";
+import { LoaderCircle } from "lucide-react";
 
 export default function NewCourse() {
   const router = useRouter()
 
   const [loading, setLoading] = useState(false)
+  const [languages, setLanguages] = useState<Language[]>([])
+
+  async function loadData() {
+    try {
+      setLoading(true)
+      const response = await fetch('/api/language')
+      const data = await response.json()
+      setLanguages(data)
+    } catch {
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    loadData()
+  }, [])
 
   async function onSubmit(formData: z.infer<typeof CourseFormSchema>) {
     try {
       setLoading(true)
+      const { title, description, targetAudience, learningObjectives, level, duration } = formData
       const response = await fetch('/api/course', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify({
+          title,
+          description,
+          targetAudience,
+          learningObjectives,
+          level,
+          duration,
+          languages: formData.languages.map((l) => ({ id: l }))
+        })
       })
       const data = await response.json()
-      router.push(`/courses/${data.id}/outline`)
+      router.push(`/courses/${data.id}/topics`)
     } catch {
 
     } finally {
@@ -30,11 +58,13 @@ export default function NewCourse() {
   }
 
   return (
-    <div className="container mx-auto">
-      <div className="max-w-[500px] mx-auto flex flex-col gap-4">
-        <h1 className="text-xl font-semibold">New Course</h1>
-        <CourseForm loading={loading} submit={onSubmit} />
-      </div>
+    <div className="flex flex-col gap-4 max-w-[480px]">
+      <h1 className="text-xl font-semibold">New Course</h1>
+      {loading && !languages.length ? (
+        <LoaderCircle className="h-6 w-6 animate-spin" />
+      ) : (
+        <CourseForm loading={loading} languages={languages} submit={onSubmit} />
+      )}
     </div>
   );
 }
