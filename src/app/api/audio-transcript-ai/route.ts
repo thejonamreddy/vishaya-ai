4c2bb50612cbfbdb256f7c5a8c1764f6746c54e9
Manoj Kumar Reddy Pollagari <thejonamreddy@gmail.com>
2024-10-02 13:25:45 +0530

    Refacoter audio transcript ai

---
+++ src/app/api/audio-transcript-ai/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server"
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { Topic } from "@/app/interfaces/topic";
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_CLOUD_API_KEY || "");
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+export async function POST(req: NextRequest) {
+  const { title, description } = await req.json() as Topic
+
+  const prompt = `
+    Provide a conversational-style transcript on ${title}, as if you were explaining it verbally to someone. The tone should be friendly and engaging, like you're speaking directly to an audience.
+
+    Here is the summary of the topic for better context - ${description}
+
+    Output only plain text. Do not output markdown.
+  `;
+
+  const result = await model.generateContent(prompt);
+  return NextResponse.json(result.response.text(), { status: 200 });
+}
